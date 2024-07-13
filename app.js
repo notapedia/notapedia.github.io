304b473db0d1e47ad4c2e35af243f55d5ad766aa
notapedia <aagrawal4@gmail.com>
2024-07-13 19:13:04 +0400

    skibidi changes

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+
+var search = window.location.search.substr(1)
+
+if(search){
+    var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvslots=*&rvprop=content&formatversion=2&titles=" + search;
+
+    fetch(api)
+    .then(response => response.json());
+    .then(response => {
+        response = response.query.pages;
+        var pageid = Object.keys(response)[0];
+        var extract = response[pageid].extract;
+        
+        title.innerHTML = search[0].toUpperCase() + search.substring(1).toLowerCase();
+        content.innerHTML = extract;
+    })
+}
