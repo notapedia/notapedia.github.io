1d07d0980f624773d9a152bfe002e7ce9e94288b
notapedia <aagrawal4@gmail.com>
2024-07-13 19:28:32 +0400

    added event listene code in index

---
+++ index.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("search-input")
+
+input.addEventListener("keyup", function(e){
+    if(e.keyCode === 13){
+        var value = input.value;
+        if(value){
+            window.location.href = 'reader.html?' + value;
+        }
+    }
+})
