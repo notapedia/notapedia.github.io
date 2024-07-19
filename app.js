c1d5339d1402bd1ee5dbfef245b1d75e4677bcb4
notapedia <aagrawal4@gmail.com>
2024-07-19 15:54:00 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-
 const title = document.getElementById("title");
 const content = document.getElementById("content");
 
 var search = window.location.search.substr(1);
 
 if(search){
-    var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvslots=*&rvprop=content&formatversion=2&titles=" + search;
-
-    fetch(api)
-    .then(response => response.json())
-    .then(response => {
-        response = response.query.pages;
-        var pageid = Object.keys(response)[0];
-        var extract = response[pageid].extract;
+    var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&rvslots=*&rvprop=content&formatversion=2&titles=" + search;
+    
+    fetch(api).then(response => response.json()).then(response => {
         
-        title.innerHTML = search[0].toUpperCase() + search.substring(1).toLowerCase();
-        content.innerHTML = extract;
+        var heading = response.query.pages[0].title;
+        var api = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page="+encodeURIComponent(heading);
+        title.innerHTML = heading;
+        fetch(api).then(response => response.json()).then(response => {
+            content.innerHTML = response.parse.text['*'];
+        })
     })
 }
